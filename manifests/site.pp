a48f52c4d91d0cf9df482f2aff2510f17fe6fb23
Flannon Jackson <flannon@nyu.edu>
2017-02-25 21:52:30 -0500

    ...

---
+++ manifests/site.pp
@@ -0,0 +1,63 @@
+#require boxen::environment
+#require homebrew
+#require gcc 
+
+#Exec {
+#  group       => 'staff',
+#  logoutput   => on_failure,
+#  user        => $boxen_user,
+#
+#  path => [
+#    "${boxen::config::home}/rbenv/shims",
+#    "${boxen::config::home}/rbenv/bin",
+#    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
+#    "${boxen::config::homebrewdir}/bin",
+#    '/usr/bin',
+#    '/bin',
+#    '/usr/sbin',
+#    '/sbin'
+#  ],  
+#
+#  environment => [
+#    "HOMEBREW_CACHE=${homebrew::config::cachedir}",
+#    "HOME=/Users/${::boxen_user}"
+#  ]
+#}
+
+#File {
+#  group => 'staff',
+#  owner => $boxen_user
+#}
+
+Package {
+  provider => homebrew,
+  require  => Class['homebrew']
+}
+
+Repository {
+  provider => git,
+  extra    => [
+    '--recurse-submodules'
+  ],
+#  require  => File["${boxen::config::bindir}/boxen-git-credential"],
+#  config   => {
+#    'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
+#  }
+}
+
+Service {
+  provider => ghlaunchd
+}
+
+Homebrew::Formula <| |> -> Package <| |>
+
+
+node /kenny/ {
+
+  include roles::kenny
+
+}
+
+node default {
+  include roles
+}
